bdb7a11991213f35bb789c06b9180d9260d7e39f
Wel3Com <76917316+Wel3Com@users.noreply.github.com>
2022-04-25 19:11:35 -0500

    Added all Cesium Archives
    Every Cesium build except for 5-5.2

---
+++ Primrose/login/index.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const app = express()
+const session = require('express-session')
+const bodyParser = require('body-parser')
+
+let PP = {
+    session: 
+    	session({
+			secret: 'credentials',
+			resave: false,
+			saveUninitialized: true,
+			unset: 'destroy',
+			name: 'credentials',
+			cookie: {
+			}
+		}),
+    configure: 
+    	function(options) {
+    		return function(req, res, next) {
+    			if(!req.session.PP) {
+    				req.session.PP = {
+            username: process.env.UNAME,
+            password: process.env.AUTH,
+    				loggedIn: false
+    				}
+    			}
+				next()    			
+    		}
+    	},    
+    login: 
+    	function(req, res, next) {
+    		if(req.session.PP && req.session.PP.loggedIn) {
+    			next()
+    		}
+			else if(req.body && (req.body.uname == req.session.PP.username) && (req.body.pass == req.session.PP.password)) {
+				req.session.PP.loggedIn = true
+				next()
+			}
+			// deny access
+			else {
+        res.sendFile(__dirname + "/" + "login.html");
+			}		
+
+    	}
+}
+
+module.exports = function(options) {
+  return [bodyParser.urlencoded({extended: true}), PP.session, PP.configure(options), PP.login]
+}
