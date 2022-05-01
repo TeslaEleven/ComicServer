d64c8cf51f076d2d2cf81d15d52e781df63f8d7c
Wel3Com <76917316+Wel3Com@users.noreply.github.com>
2022-04-30 21:41:13 -0500

    Update AddFeed.js

--- js/AddFeed.js
+++ js/AddFeed.js
@@ -24,7 +24,7 @@ function handleGotoFeed() {
   var old = localStorage.getItem('oldkey');
   localStorage.setItem('userFeedURLs', old + "," + userFeedURLs);
   }
-    window.location.href = './';
+    window.location.href = '../';
 }
 
 function addFeedURL(url) {
