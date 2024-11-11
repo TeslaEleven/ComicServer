8b6a69854b1e21304545ddebe5954ac3a3b9341e
TeslaEleven <76917316+TeslaEleven@users.noreply.github.com>
2024-11-10 20:33:22 -0600

    Update els.js

--- js/wd24/els.js
+++ js/wd24/els.js
@@ -1,202 +1,197 @@
+// Christmas Light Smashfest
+
 function $(sID) {
   return document.getElementById(sID);
 }
 
+// Define utility functions for animations and easing
+function animate(element, properties, duration, easing, onComplete) {
+  const startValues = {};
+  const deltas = {};
+  const startTime = performance.now();
+
+  for (const prop in properties) {
+    startValues[prop] = parseFloat(getComputedStyle(element)[prop]) || 0;
+    deltas[prop] = properties[prop] - startValues[prop];
+  }
+
+  function step() {
+    const elapsedTime = performance.now() - startTime;
+    const progress = Math.min(elapsedTime / duration, 1);
+    const ease = easing ? easing(progress) : progress;
+
+    for (const prop in properties) {
+      element.style[prop] = startValues[prop] + deltas[prop] * ease + "px";
+    }
+
+    if (progress < 1) {
+      requestAnimationFrame(step);
+    } else if (onComplete) {
+      onComplete();
+    }
+  }
+  requestAnimationFrame(step);
+}
+
+// Easing function for a smooth animation
+const easeOutStrong = t => 1 - Math.pow(1 - t, 3);
+
 function XLSF(oTarget, urlBase) {
-  var urlBase = (urlBase ? urlBase : 'lights/');
-  var animDuration = 1;
+  const url = urlBase || 'lights/';
   this.oFrag = document.createDocumentFragment();
-  this.oTarget = (oTarget ? oTarget : document.documentElement);
+  this.oTarget = oTarget || document.documentElement;
   this.oExplosionBox = document.createElement('div');
   this.oExplosionBox.className = 'xlsf-fragment-box';
   this.oExplosionFrag = document.createElement('div');
   this.oExplosionFrag.className = 'xlsf-fragment';
   this.lights = [];
-  this.lightClasses = {
-    pico: 32,
-    tiny: 50,
-    small: 64,
-    medium: 72,
-    large: 96
-  };
+  this.lightClasses = { pico: 32, tiny: 50, small: 64, medium: 72, large: 96 };
 
-  if (window.innerWidth || window.innerHeight) {
-    var screenX = window.innerWidth;
-    var screenY = window.innerHeight;
-  } else {
-    var screenX = (document.documentElement.clientWidth || document.body.clientWidth || document.body.scrollWidth);
-    var screenY = (document.documentElement.clientHeight || document.body.clientHeight || document.body.scrollHeight);
-  }
-
-  this.lightClass = (screenX > 1800 ? 'small' : 'pico');
-  if (window.location.href.match(/size=/i)) {
-    this.lightClass = window.location.href.substr(window.location.href.indexOf('size=') + 5);
-  }
+  // Determine screen size and light class
+  const screenX = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+  const screenY = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+  this.lightClass = screenX > 1800 ? 'small' : 'pico';
   this.lightXY = this.lightClasses[this.lightClass];
 
-  this.lightGroups = {
-    left: [],
-    top: [],
-    right: [],
-    bottom: []
-  };
+  this.lightGroups = { left: [], top: [], right: [], bottom: [] };
   this.lightSmashCounter = 0;
   this.lightIndex = 0;
   this.lightInterval = 500;
   this.timer = null;
-  this.bgBaseX = 0;
-  this.bgBaseY = 0;
-  this.soundIDs = 0;
-  this.soundPan = {
-    panValue: 75,
-    left: 0,
-    mid: 481,
-    right: 962
-  };
 
-  this.cover = document.createElement('div');
-  this.cover.className = 'xlsf-cover';
-  document.documentElement.appendChild(this.cover);
-
-  this.initSounds = function() {
-    for (var i = 0; i < 6; i++) {
+  // Sounds setup
+  this.initSounds = function () {
+    for (let i = 0; i < 6; i++) {
       soundManager.createSound({
-        id: 'smash' + i,
-        url: urlBase + 'sound/glass' + i + '.mp3',
+        id: `smash${i}`,
+        url: `${url}sound/glass${i}.mp3`,
         autoLoad: true,
         multiShot: true,
         volume: 50
       });
     }
-    self.initSounds = function() {};
   };
 
-  this.appendLights = function() {
-    self.oTarget.appendChild(self.oFrag);
-    self.oFrag = document.createDocumentFragment();
+  // Helper for appending lights
+  this.appendLights = function () {
+    this.oTarget.appendChild(this.oFrag);
+    this.oFrag = document.createDocumentFragment();
   };
 
+  // Explosion Fragment class
   function ExplosionFragment(nType, sClass, x, y, vX, vY) {
-    var self = this;
-    this.o = xlsf.oExplosionFrag.cloneNode(true);
+    this.o = this.oExplosionFrag.cloneNode(true);
     this.nType = nType;
-    this.sClass = sClass;
     this.x = x;
     this.y = y;
-    this.w = 50;
-    this.h = 50;
     this.vX = vX * (1.5 + Math.random());
     this.vY = vY * (1.5 + Math.random());
-    this.o.style.backgroundPosition = ((this.w * -1) + 'px ' + (this.h * -nType) + 'px');
+    this.burstPhase = 1;
+    this.burstPhases = 4;
 
-    this.burst = function() {
-      // Simulate an animation for the explosion
-      var marginLeft = self.vX * 8 + 'px';
-      var marginTop = self.vY * 8 + 'px';
-      self.o.style.transition = `margin ${animDuration}s ease-out`;
-      self.o.style.marginLeft = marginLeft;
-      self.o.style.marginTop = marginTop;
+    this.burst = function () {
+      animate(this.o, { marginLeft: this.vX * 8, marginTop: this.vY * 8 }, 1000, easeOutStrong, () => {
+        this.reset();
+      });
     };
 
-    this.hide = function() {
-      self.o.style.opacity = 0;
-    };
-
-    this.animate = function() {
-      self.burst();
+    this.reset = function () {
+      this.o.style.left = '0px';
+      this.o.style.top = '0px';
+      this.o.style.marginLeft = '0px';
+      this.o.style.marginTop = '0px';
     };
   }
 
-  function Light(sSizeClass, sClass, nType, x, y) {
-    var self = this;
+  // Explosion class
+  function Explosion(nType, sClass, x, y) {
+    this.o = this.oExplosionBox.cloneNode(true);
+    this.o.style.left = `${x}px`;
+    this.o.style.top = `${y}px`;
+    this.fragments = [];
+
+    const fragmentOffsets = [
+      [-5, -5], [0, -5], [5, -5],
+      [-5, 0], [0, 0], [5, 0],
+      [-5, 5], [0, 5], [5, 5]
+    ];
+    for (let [vx, vy] of fragmentOffsets) {
+      this.fragments.push(new ExplosionFragment(nType, sClass, x, y, vx, vy));
+    }
+  }
+
+  // Light class
+  function Light(sClass, nType, x, y) {
     this.o = document.createElement('div');
     this.sClass = sClass;
-    this.sSizeClass = sSizeClass;
-    this.nType = (nType || 0);
     this.state = null;
     this.broken = 0;
-    this.w = xlsf.lightClasses[sSizeClass];
-    this.h = xlsf.lightClasses[sSizeClass];
     this.x = x;
     this.y = y;
-    this.o.style.width = this.w + 'px';
-    this.o.style.height = this.h + 'px';
-    this.o.style.background = `url(${urlBase}image/bulbs-${this.w}x${this.h}-${this.sClass}.png) no-repeat`;
-
-    this.smash = function(e) {
-      if (self.broken) return false;
-      self.broken = true;
-      if (soundManager && soundManager.ok()) {
-        soundManager.play(self.soundID);
-      }
-      self.o.style.backgroundPosition = `-${self.w * 2}px 0px`;
-      xlsf.lightSmashCounter++;
+    this.soundID = `smash${Math.floor(Math.random() * 6)}`;
+    
+    this.setLight = function (isOn) {
+      if (this.broken || this.state === isOn) return;
+      this.state = isOn;
+      this.o.style.backgroundPosition = isOn ? '0px 0px' : '-32px 0px';
     };
-
-    this.init = function() {
-      self.o.className = `xlsf-light ${this.sizeClass} ${this.sClass}`;
-      self.o.style.left = self.x + 'px';
-      self.o.style.top = self.y + 'px';
-      self.o.addEventListener('mouseover', self.smash);
-      self.o.addEventListener('click', self.smash);
-      xlsf.oFrag.appendChild(self.o);
+    
+    this.on = () => this.setLight(true);
+    this.off = () => this.setLight(false);
+    
+    this.smash = () => {
+      if (this.broken) return;
+      this.broken = true;
+      soundManager.play(this.soundID);
     };
-
-    this.init();
+    
+    this.o.onclick = this.smash;
+    this.o.onmouseover = this.smash;
+    this.setLight(Math.random() > 0.5);
+    this.oFrag.appendChild(this.o);
   }
-
-  this.createLight = function(sClass, nType, x, y) {
-    var oLight = new Light(self.lightClass, sClass, nType, x, y);
-    self.lightGroups[sClass].push(oLight);
-    self.lights.push(oLight);
-    return oLight;
+  
+  this.createLight = function (sClass, nType, x, y) {
+    const light = new Light(sClass, nType, x, y);
+    this.lightGroups[sClass].push(light);
+    this.lights.push(light);
+    return light;
   };
-
-  this.randomLights = function() {
-    self.lights[parseInt(Math.random() * self.lights.length)].toggle();
+  
+  this.randomLights = function () {
+    this.lights[Math.floor(Math.random() * this.lights.length)].toggle();
   };
-
-  this.startSequence = function(fSequence, nInterval) {
-    if (self.timer) self.stopSequence();
-    self.timer = window.setInterval(fSequence, nInterval ? nInterval : self.lightInterval);
-  };
-
-  this.stopSequence = function() {
-    if (self.timer) {
-      clearInterval(self.timer);
-      self.timer = null;
+  
+  this.destroyLight = function () {
+    if (this.lightSmashCounter < this.lights.length) {
+      this.lights[this.lightSmashCounter].smash();
+      this.lightSmashCounter++;
+    } else {
+      clearInterval(this.timer);
     }
   };
-
-  var jMax = Math.floor((screenX - 16) / self.lightXY);
-  for (var j = 0; j < jMax; j++) {
-    this.createLight('top', parseInt(j / 3) % 4, j * self.lightXY, 0);
+  
+  this.startSequence = function () {
+    this.timer = setInterval(() => this.randomLights(), this.lightInterval);
+  };
+  
+  $('lights').style.display = 'block';
+  for (let j = 0; j < Math.floor(screenX / this.lightXY); j++) {
+    this.createLight('top', j % 4, j * this.lightXY, 0);
   }
   this.appendLights();
-  this.startSequence(self.randomLights);
+  this.startSequence();
 }
 
-var xlsf = null;
-
 function smashInit() {
-  if (navigator.userAgent.match(/msie 6/i)) {
-    return false;
-  }
-  xlsf = new XLSF(document.getElementById('lights'));
-  if ($('loading')) {
-    $('loading').style.display = 'none';
-  }
-  xlsf.initSounds();
+  if (navigator.userAgent.includes('msie 6')) return;
+  new XLSF(document.getElementById('lights'));
 }
 
 soundManager.setup({
   flashVersion: 9,
   preferFlash: false,
   url: 'lights/',
-  onready: function() {
-    smashInit();
-  },
-  ontimeout: function() {
-    smashInit();
-  }
+  onready: smashInit,
+  ontimeout: smashInit
 });
